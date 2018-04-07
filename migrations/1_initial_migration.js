582a0529dba263ad09f7f47f4940dc40d4afc099
shysi <haeyong.shin@codecrain.com>
2018-04-08 00:31:29 +0900

    180407

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,16 @@
+const Web3 = require('web3');
+const TruffleConfig = require('../truffle');
 var Migrations = artifacts.require("./Migrations.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = (deployer, network, addresses) => {
+    const config = TruffleConfig.networks[network];
+    module.exports = (deployer, network, addresses) => {
+        const config = TruffleConfig.networks[network];
+        const web3 = new Web3(new Web3.providers.HttpProvider('http://' + config.host + ':' + config.port));
+        web3.personal.unlockAccount(config.from, process.env.ACCOUNT_PASSWORD, 36000);
+
+        deployer.deploy(Migrations);
+    };
+
+    deployer.deploy(Migrations);
 };
--- truffle.js
+++ truffle.js
@@ -4,8 +4,9 @@ module.exports = {
   // truffle.cmd compile을 하면 정상적으로 실행됨
     networks: {
         development: {
+            from: "77293f02b7d7c442078bee2e319ed8d75d75ad7f",
             host: "localhost",
-            port: 8545,
+            port: 8123,
             network_id: "*" // Match any network id
         }
     }
